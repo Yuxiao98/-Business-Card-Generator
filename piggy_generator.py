bada671f7aaad18f4673cc7857c7f39ba8eeb32e
Yuxiao Chen <46464820+Yuxiao98@users.noreply.github.com>
2019-11-18 13:57:35 -0500

    please give it a try and you won't regret it ;^)

---
+++ piggy_generator.py
@@ -0,0 +1,29 @@
+name = input ("Enter name :")
+
+if len(name) >= 30 or len(name) == 0:
+    print("Ooops! That's not good for piggy!>_<")
+    exit()
+
+elif len(name)%2 == 0:
+    add_spaces = 30 - len(name)
+    name_length=len(name)+add_spaces    # will be adding 10 extra spaces
+    name=name.center(name_length)
+    print(name)
+
+else:
+    front_spaces = (30 - len(name))//2
+    back_spaces = front_spaces + 1
+    back_length=len(name)+back_spaces
+    name=name.ljust(back_length)
+    front_length=len(name)+front_spaces
+    name=name.rjust(front_length)
+    print(name)
+
+
+print('   M_________________________')
+print("  /                          \\")
+print(" /   .                        \\")
+print(f"O{name}Q")
+print(" \\                            /")
+print("  \\  ______________________  / ")
+print("    W                       W")
