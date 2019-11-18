391680920fdead78962456203cb5437047f7efc8
Yuxiao Chen <46464820+Yuxiao98@users.noreply.github.com>
2019-11-18 14:23:14 -0500

    WAY MORE SIMPLE

--- piggy_generator.py
+++ piggy_generator.py
@@ -4,18 +4,7 @@ if len(name) >= 30 or len(name) == 0:
     print("Ooops! That's not good for piggy!>_<")
     exit()
 
-elif len(name)%2 == 0:
-    add_spaces = 30 - len(name)
-    name_length=len(name)+add_spaces  
-    name=name.center(name_length)
-
-else:
-    front_spaces = (30 - len(name))//2
-    back_spaces = front_spaces + 1
-    back_length=len(name)+back_spaces
-    name=name.ljust(back_length)
-    front_length=len(name)+front_spaces
-    name=name.rjust(front_length)
+name = name.center(30)
 
 print('   M_________________________')
 print("  /                          \\")
