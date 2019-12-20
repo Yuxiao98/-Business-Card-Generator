b4480687845075253b55bc1ea4aa8dfb3328b556
Yuxiao Chen <46464820+Yuxiao98@users.noreply.github.com>
2019-12-20 10:28:58 -0500

    Replaced Mandarin expressions into English

--- wx.py
+++ wx.py
@@ -15,8 +15,8 @@ def send_news():
         t = Timer(86400, send_news)
         t.start()
     except:
-        my_friend = bot.friends().search(u'箫洒哥')[0]
-        my_friend.send(u"今天消息发送失败了")
+        my_friend = bot.friends().search(u'my WeChat name')[0]
+        my_friend.send(u"Failed to send the message")
 
 if __name__ == "__main__":
     send_news()
